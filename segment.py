4d75253b85c871cf826be7c4482c716d0ea581b2
Marek Pokropiński <marek.pokropinski@outlook.com>
2020-10-20 15:13:05 +0200

    improved segmenting

--- segment.py
+++ segment.py
@@ -1,47 +1,98 @@
 import re
 sentence_expression = r"([A-Z][^\.^\?^\!]*[\.\?\!])"
 
+sentence_exceptions = [
+    (r'([Dd]r\. $)', r'(.+)'),
+    (r'([Mm]gr\. $)', r'(.+)'),
+    (r'([Ii]nż\. $)', r'(.+)'),
+    (r'([Tt]zn\. $)', r'(.+)'),
+    (r'([Tt]z\. $)', r'(.+)'),
+]
+
+sentence_rules = [
+    (r'([\!\?]\s)$', r'(.+)'),
+    (r'(\.\s)$', r'([A-ZŻŹĆŃÓŁĘĄŚ])'),
+]
+
+sentence_rules = [(re.compile(rule[0]), re.compile(rule[1]))
+                  for rule in sentence_rules]
+sentence_exceptions = [(re.compile(rule[0]), re.compile(rule[1]))
+                       for rule in sentence_exceptions]
+
 with open('text.txt', encoding='utf-8') as t:
     text = t.read()
 
-# text = input()
-matches = re.findall(sentence_expression, text)
+splits = [0]
+pos = 0
 
-sentences = []
+while pos < len(text):
+    for exception in sentence_exceptions:
+        if exception[0].search(text[:pos]) and exception[1].match(text, pos):
+            break
+    else:
+        # no exception hit
+        for rule in sentence_rules:
+            if rule[0].search(text[:pos]) and rule[1].match(text, pos):
+                splits.append(pos)
+                break
+    pos += 1
+
+splits.append(pos)
+
+for a, b in zip(splits, splits[1:]):
+    print(text[a:b])
+
+sentences = [text[a:b].rstrip() for a, b in zip(splits, splits[1:])]
 print("Zdania:")
-for i, match in enumerate(matches):
-    sentences.append(match.lower())
-    print(f'{i}: "{match}"')
+for i, sentence in enumerate(sentences):
+    print(f'{i}: "{sentence}"')
 
-words_expressions = [
-    r'([a-zżźćńółęąś]+)'
-]
-symbols = [
-    r'([\-–\+,\'\"\:;\?\.\!])'
+# words_expressions = [
+#     r'([a-zżźćńółęąś]+)'
+# ]
+# symbols = [
+#     r'([\-–\+,\'\"\:;\?\.\!])'
+# ]
+
+word_exceptions = [
+    (r'([Dd]r$)', r'(\.)'),
+    (r'([Mm]gr$)', r'(\.)'),
+    (r'([Ii]nż$)', r'(\.)'),
+    (r'([Tt]zn$)', r'(\.)'),
+    (r'([Tt]z$)', r'(\.)'),
+    (r'([Ii]td$)', r'(\.)'),
 ]
 
+word_rules = [
+    (r'([A-ZŻŹĆŃÓŁĘĄŚa-zżźćńółęąś1-9]) $', r'([A-ZŻŹĆŃÓŁĘĄŚa-zżźćńółęąś1-9])'),
+    (r'([a-zżźćńółęąś1-9])$', r'([,\.;\!\?–\-\*\/\+\^\&])'),
+    (r'([,\.;\!\?–\-\*\/\+\^\&])$', r'.?'),
+]
+
+word_rules = [(re.compile(rule[0]), re.compile(rule[1]))
+                  for rule in word_rules]
+word_exceptions = [(re.compile(rule[0]), re.compile(rule[1]))
+                       for rule in word_exceptions]
 
 for sentence in sentences:
-    print(sentence)
-    tokens = []
-    while sentence:
-        matched = False
-        for reg_ex in  words_expressions:
-            if m := re.match(reg_ex, sentence):
-                tokens.append({'type':'word', 'text': m.group()})
-                sentence = sentence[m.end():]
-                matched = True
+    print('Sentence:', sentence)
+    splits = [0]
+    pos = 0
+
+    while pos < len(sentence):
+        for exception in word_exceptions:
+            if exception[0].search(sentence[:pos]) and exception[1].match(sentence, pos):
                 break
-        else: 
-            if not matched:
-                for reg_ex in symbols:
-                    if m := re.match(reg_ex, sentence):
-                        tokens.append({'type':'symbol', 'text': m.group()})
-                        sentence = sentence[m.end():]
-                        matched = True
-                        break
-        if not matched:
-            sentence = sentence[1:]
+        else:
+            # no exception hit
+            for rule in word_rules:
+                if rule[0].search(sentence[:pos]) and rule[1].match(sentence, pos):
+                    splits.append(pos)
+                    break
+        pos += 1
 
-    print(tokens)
-
+    splits.append(pos)
+    words = [sentence[a:b].rstrip() for a, b in zip(splits, splits[1:])]
+    print("Wyrazy:")
+    for i, word in enumerate(words):
+        print(f'{i}: "{word}"')
